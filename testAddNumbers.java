61f65260384205920eb19035067d5a88b34b9096
781-progjj <92675551+781-progjj@users.noreply.github.com>
2022-11-17 14:41:02 +0530

    Add files via upload

---
+++ testAddNumbers.java
@@ -0,0 +1,16 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitTestFunctions junit = new jUnitTestFunctions();
+		int result = junit.testaddNumbers(55,56);
+		assertEquals(111,result);
+	}
+
+}
